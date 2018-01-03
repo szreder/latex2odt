fb9b2881d65b11dd337c852f6b676a1213d8b928
Bartosz Szreder <bartosz.szreder@huuugegames.com>
2018-01-03 08:42:04 +0100

    Remove debugging code for now.

--- odtgen.cpp
+++ odtgen.cpp
@@ -7,20 +7,6 @@
 #include "Strings.hpp"
 #include "XmlGen.hpp"
 
-namespace {
-QTextStream debug_output{stderr};
-
-}
-
-void debugOutput(const Node *n, int indent = 0)
-{
-	for (int i = 0; i < indent; ++i)
-		debug_output << '\t';
-	debug_output << n->toString() << '\n';
-	for (const Node *child : n->children)
-		debugOutput(child, indent + 1);
-}
-
 int main(int argc, char *argv[])
 {
 	QTextStream input{stdin};
